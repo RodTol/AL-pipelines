21dbc57261703b8953ed73515c3ff519d000e432
RodTol <rodolfo.tolloi@gmail.com>
2024-05-06 17:55:21 +0200

    first try

--- AL_scripts/instructions.sh
+++ AL_scripts/instructions.sh
@@ -10,18 +10,34 @@ RESET="\033[0m"
 json_file=$1
 my_index=$2
 
-# Read from config.json file (necessary)
-host_index=$(jq -r '.Resources.index_host' "$json_file")
-node_name=$(jq -r --argjson my_index "$my_index" '.Resources.nodes_list[$my_index]' "$json_file")
-node_queue=$(jq -r --argjson my_index "$my_index" '.Resources.nodes_queue[$my_index]' "$json_file")
+# Read from config.json file 
+threads=$(jq -r --argjson my_index "$my_index" '.Alignment.threads[$my_index]' "$json_file")
+input_file=$(jq -r '.Alignment.input_file' "$json_file") 
+output_file=$(jq -r '.Alignment.output_file' "$json_file") 
+logs_dir=$(jq -r '.Alignment.logs_dir' "$json_file") 
+reference_genome=$(jq -r '.Alignment.reference_genome' "$json_file")
 
-gpus_settings=$(jq -r --argjson my_index "$my_index" '.Resources.gpus[$my_index]' "$json_file") #debug
+node_queue=$(jq -r --argjson my_index "$my_index" '.Resources.nodes_queue[$my_index]' "$json_file")
+node_name=$(jq -r --argjson my_index "$my_index" '.Resources.nodes_list[$my_index]' "$json_file")
 
 # Brief output for checking everything it's correct
+# Update config.json file
+if  [ "$node_name" == "" ]; then
+  echo -e "${RED}|||Update node_name from $node_name to $SLURM_NODELIST|||${RESET}"
+  node_name=$SLURM_NODELIST
+fi
 echo -e "${RED}I am this node_name: $node_name${RESET}, and for Slurm: $SLURM_NODELIST"
-echo $CUDA_VISIBLE_DEVICES
-echo -e "${RED}GPUs selected: $gpus_settings${RESET}"
 echo -e "${RED}-----------------------${RESET}"
+echo "Threads to be used: $threads"
+echo "Reference genome: $reference_genome"
 echo "Node queue: $node_queue"
+echo "Logs Directory: $logs_dir"
+echo "Input Directory: $input_file"
+echo "Output Directory: $output_file"
 echo -e "${RED}-----------------------${RESET}"
-ip a
+
+# Load virtualenv for Python:
+# None for now
+
+# Start the alignment
+minimap2 -t $threads -a -x map-ont $reference_genome  $input_file -o $output_file
